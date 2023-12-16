2e95054c2760b82b185f13e5d46fc8cbcf726701
minh-ctran <113205948+minh-ctran@users.noreply.github.com>
2023-12-16 11:44:21 -0500

    Update paddle.v

--- paddle.v
+++ paddle.v
@@ -7,21 +7,16 @@ module paddle (
 	output reg [8:0] x
 );
 
-reg play;
-
 always @(posedge clk or negedge rst)
 begin
 	if (rst == 1'b0) 
 		x <= 290;
-		play <= 0;
-	else
+	else if (start == 1'b1)
 	begin
-		if (start == 1'b0)
-			play <= 1;
 		// Paddle movement logic
-		if (left == 1'b0 && x > 134 && play == 1'b1)
+		if (left == 1'b0 && x > 134)
 			 x <= x - 1;
-		else if (right == 1'b0 && x + 62 < 506 && play == 1'b1)
+		else if (right == 1'b0 && x + 62 < 506)
 			 x <= x + 1;
 	end
 end
